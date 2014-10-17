ca44a510a3fb91761dae9a36d0895c80f84423e8
BE Design <info@b-ed.com>
2014-10-17 12:58:43 -0700

    added new font and text tweaks

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
 					<div class="inner">
 						<h1>Welcome</h1>
 						<div class="divider-top"></div>
-						<p><em>Kanin Soffer, LLP</em> is a Beverly Hills-based law firm that advises clients on estate planning, charitable planning, probate and trust administration.  We offer clients the <em>personal service</em> they expect of a specialized boutique firm, while providing the quality of work and attention to detail of highly trained attorneys with decades of combined experience.  Our clients range from high-net-worth families that require guidance on all aspects of transfer tax planning to new families seeking to establish foundational documents.  We strive to establish long-lasting relationships with our clients and instill in them the <em>trust & confidence</em> to consider us their family counselors. </p>
+						<p><em>Kanin Soffer, LLP</em> is a Beverly Hills-based law firm that advises clients on estate planning, charitable planning, probate and trust administration. We offer clients the <em>personal service</em> they expect of a specialized boutique firm, while providing the quality of work and attention to detail of highly trained attorneys with decades of combined experience. Our clients range from those new to the process looking to establish foundational documents, to high-net-worth families and individuals that require guidance on all aspects of transfer tax planning. We strive to establish long-lasting relationships with our clients and instill in them the <em>trust & confidence</em> to consider us their family counselors. </p>
 						<div class="divider-bot"></div>
 					</div>
 				</div>
@@ -65,19 +65,19 @@
 						<h1>Services</h1>
 						<div class="divider-top"></div>
 						<h2>Estate Planning</h2>
-						<p>At Kanin Soffer, LLP, we draft estate plans that address each client’s individual circumstances.  A well-executed estate plan can help families avoid unwanted probate and conservatorships, protect clients from paying unnecessary taxes and allow them to design and implement strategic approaches that best fit their unique needs and objectives.</p>  
+						<p>At Kanin Soffer, LLP, we draft estate plans that address each client’s individual circumstances. A well-executed estate plan can help families avoid unwanted probate and conservatorships, protect clients from paying unnecessary taxes and allow them to design and implement strategic approaches that best fit their unique needs and objectives.</p> 
 
-						<p>Our estate planning services range from drafting wills and trusts for new families and business owners to preparing sophisticated strategies that maximize wealth transfer and minimize estate, gift and generation-skipping taxes.  We take the time to understand our clients’ businesses, enabling us to efficiently assist them on transferring control to future generations.  We also draft charitable remainder and charitable lead trusts for clients who wish to incorporate charitable giving into their planning. </p>
+						<p>Our estate planning services range from drafting wills and trusts for new families and business owners to preparing sophisticated strategies that maximize wealth transfer and minimize estate, gift and generation-skipping taxes. We take the time to understand our clients' businesses, enabling us to efficiently assist them on transferring control to future generations. We also draft charitable remainder and charitable lead trusts for clients who wish to incorporate charitable giving into their planning. </p>
 
 						<div class="divider-mid"></div>
 
 						<h2>Trust Administration</h2>
-						<p>Kanin Soffer, LLP works with clients on administering trusts.  We advise trustees on how to comply effectively with the trust and with the law, and we guide trustees with respect to strategic decisions that will help minimize liability.  We counsel clients throughout the process and advise clients on how to achieve the most tax efficient results. </p>
+						<p>Kanin Soffer, LLP works with clients on administering trusts. We advise trustees on how to comply effectively with the trust and with the law, and we guide trustees with respect to strategic decisions that will help minimize liability. We counsel clients throughout the process and advise clients on how to achieve the most tax efficient results. </p>
 
 						<div class="divider-mid"></div>
 
 						<h2>Probate</h2>
-						<p>The attorneys at Kanin Soffer, LLP routinely assist clients with probate.  From traditional probate proceedings to the more complex, we provide hands-on assistance throughout the process.  While probate can be a lengthy process, our office handles these matters efficiently and effectively.</p>
+						<p>The attorneys at Kanin Soffer, LLP routinely assist clients with probate. From traditional probate proceedings to the more complex, we provide hands-on assistance throughout the process. While probate can be a lengthy process, our office handles these matters efficiently and effectively.</p>
 
 						<div class="divider-bot"></div>
 					</div>
@@ -89,12 +89,12 @@
 						<h1>The Team</h1>
 						<div class="divider-top"></div>
 						<h2>Paul R. Kanin</h2>
-						<p>Paul R. Kanin is a partner at Kanin Soffer, LLP.  Paul's practice is limited to matters related to trusts and estates with an emphasis on the tax aspects of same.  Paul has achieved recognition from his peers as a Super Lawyer and has always maintained the highest possible ratings from Martindale Hubbell.  He has been a member of the Beverly Hills Bar Association for more than thirty (30) years as well as a member of the Beverly Hills Estate Planning Council.</p>
+						<p>Paul R. Kanin is a partner at Kanin Soffer, LLP. Paul's practice is limited to matters related to trusts and estates with an emphasis on the tax aspects of same. Paul has achieved recognition from his peers as a Super Lawyer and has always maintained the highest possible ratings from Martindale Hubbell. He has been a member of the Beverly Hills Bar Association for more than thirty (30) years as well as a member of the Beverly Hills Estate Planning Council.</p>
 
 						<div class="divider-mid"></div>
 
 						<h2>David Soffer</h2>
-						<p>David Soffer is a partner at Kanin Soffer, LLP.  David's practice focuses on estate and tax planning, probate and the administration of trusts and estates.  David received his undergraduate degree from the University of California at Berkeley, where he was a member of Phi Beta Kappa.   He earned both his J.D. and his Masters of Law degree (specialization in tax) from UCLA School of Law.  David previously worked at the multi-national law firms Jones Day and Proskauer Rose LLP.  He was nominated as a Southern California Super Lawyers Rising Star in 2013 and 2014.</p>
+						<p>David Soffer is a partner at Kanin Soffer, LLP. David's practice focuses on estate and tax planning, probate and the administration of trusts and estates. David received his undergraduate degree from the University of California at Berkeley, where he was a member of Phi Beta Kappa.  He earned both his J.D. and his Masters of Law degree (specialization in tax) from UCLA School of Law. David previously worked at the multi-national law firms Jones Day and Proskauer Rose LLP. He was nominated as a Southern California Super Lawyers Rising Star in 2013 and 2014.</p>
 
 						<div class="divider-bot"></div>
 					</div>
@@ -106,7 +106,7 @@
 						<div class="logo"><img src="/images/KS-logo.svg" alt="" /></div>
 						<h1>Kanin Soffer, LLP</h1>
 						<p>9350 Wilshire Boulevard, Suite 212<br />
-						Beverly Hills, CA 90212  MAP</p>
+						Beverly Hills, CA 90212</p>
 
 						<div class="divider-mid"></div>
 
@@ -122,6 +122,10 @@
 							<div class="info">
 								<div class="label">Email:</div>
 								<div class="data"><a href="mailto:info@kaninsoffer.com">info@kaninsoffer.com</a></div>
+							</div>
+							<div class="info">
+								<div class="label">Location:</div>
+								<div class="data"><a href="https://goo.gl/maps/g7wfz">Link to Map</a></div>
 							</div>
 						</div>
 					</div>
@@ -145,10 +149,10 @@
 	
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
-  <script type="text/javascript" src="js/vendor/skrollr.min.js"></script>
-  <script type="text/javascript" src="js/vendor/backstretch.js"></script>
-  <script src="js/init.js"></script>
+ <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+ <script type="text/javascript" src="js/vendor/skrollr.min.js"></script>
+ <script type="text/javascript" src="js/vendor/backstretch.js"></script>
+ <script src="js/init.js"></script>
 
 
 </html>
